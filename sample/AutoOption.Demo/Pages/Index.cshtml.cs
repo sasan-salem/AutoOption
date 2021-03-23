7081ab8bb9226ec1fc95e76bc8f2250bd51edbb9
Sasan Salem <ssn.salem@gmail.com>
2021-03-24 01:52:08 +0430

    simplify option demo

--- sample/AutoOption.Demo/Pages/Index.cshtml.cs
+++ sample/AutoOption.Demo/Pages/Index.cshtml.cs
@@ -10,16 +10,5 @@ namespace AutoOption.Demo.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
     }
 }
